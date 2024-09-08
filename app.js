34a63d37ce25e11c96d330045d1cd1fb7dd35b2d
Marcelo Izidio <132240591+MarceloIzidioo@users.noreply.github.com>
2024-09-08 11:59:28 -0300

    Initial Deploy

---
+++ app.js
@@ -0,0 +1,52 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa"); // Seleciona a seção onde os resultados serão exibidos
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    console.log(campoPesquisa)
+
+    let resultados = ""; // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa pesquisar por um atleta ou esporte</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // Itera sobre cada elemento (dado) dentro do array 'dados'
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // Constrói o HTML de cada resultado, formatando os dados do objeto 'dado'
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>`;
+        }
+
+        
+    }
+    // O operador += concatena o novo HTML à string 'resultados' a cada iteração do loop
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // Substitui o conteúdo HTML da seção 'resultados-pesquisa' pelos resultados construídos
+    section.innerHTML = resultados;
+}
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,42 @@
+let dados = [
+    {
+        titulo : "Hugo Calderano",
+        descricao : "Hugo Calderano é um mesa-tenista brasileiro que se destaca pela sua agilidade e precisão. Ele é considerado um dos melhores atletas de tênis de mesa da América Latina e já conquistou diversas medalhas em competições internacionais.",
+        link : "https://pt.wikipedia.org/wiki/Hugo_Calderano",
+        tags :"tenis de mesa ping pong"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua habilidade, determinação e carisma. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com um currículo repleto de conquistas e feitos históricos.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags :"ginastica ginástica"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destaca pela sua força e técnica. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : ""
+    },
+    {
+        titulo: "Neymar Jr",
+        descricao: "Neymar Jr é um dos maiores jogadores de futebol do mundo, conhecido por sua habilidade e dribles. Atualmente joga no Paris Saint-Germain.",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags : "futebol njr barcelona psg paris menino ney"
+    },
+    {
+        titulo: "Gabriel Medina",
+        descricao: "Gabriel Medina é um surfista profissional brasileiro, campeão mundial e um dos maiores nomes do surf mundial.lorem ipsum lorem ipsum lorem ipsum",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        tags : "surfista"
+    },
+    {
+        titulo: "Edival Pontes",
+        descricao: "Edival Pontes é um atleta de taekwondo brasileiro, conhecido por sua técnica e força. Ele já conquistou diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Edival_Pontes", // Substitua por um link real
+        tags : "taekwondista netinho"
+    }
+];
+
+
+
+
